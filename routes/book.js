37be3744047dd4320e610c49c8cf163db7381d31
Ola Abdallah <67606039+OlaAbdallah1@users.noreply.github.com>
2022-08-08 15:18:39 +0300

    notify & invalidate + replication

--- routes/book.js
+++ routes/book.js
@@ -1,7 +1,23 @@
 const express = require('express')
 const Book = require('../models/book')
+const invalidate = require('../utils/invalidate')
 const router = express.Router()
+const notify = require('../utils/notify')
 const log = require('../middleware/log')
+
+//add a book
+router.post('/books',log, (req, res)=>{
+    const book = new Book (req.body)
+    book.save().then(()=>{
+        res.send(book)
+    }).then((error)=>{
+        res.status(400).send(error)
+    })
+})
+
+router.get('/',log, (req, res)=>{
+	res.send('heeey')
+})
 
 //get a book by number 
 router.get('/books/info/:id', log, async(req, res)=>{
@@ -32,8 +48,9 @@ router.get('/books/search/:topic', log, async(req, res)=>{
     }
 })
 
-//update cost / number of items
-router.patch('/books/:id', async (req, res) => {
+
+//update cost or number of items
+router.patch('/books/:id', log, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['cost', 'numberOfItems']
     try {
@@ -47,13 +64,39 @@ router.patch('/books/:id', async (req, res) => {
         }
 
         updates.forEach((update) => { book[update] = req.body[update] })
-
         await book.save()
+        await notify(req.params.id, req.body)
+        await invalidate(req.params.id)
         res.status(200).send("Book was successfully updated")
     } catch (e) {
-        res.status(500).send()
+        res.status(500).send(e)
     }
+
 })
 
-module.exports= router
 
+//got a notification
+router.patch('/notify/books/:id', log, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['cost', 'numberOfItems']
+    try {
+        const book = await Book.findOne({ itemNumber: req.params.id })
+        if (!book) {
+            return res.status(404).send("Invalid Item Id")
+        }
+        const isValidUpdate= updates.every((update) => allowedUpdates.includes(update))
+        if (!isValidUpdate) {
+            return res.status(400).send( "invalid update operation")
+        }
+
+        updates.forEach((update) => { book[update] = req.body[update] })
+        await book.save()
+        console.log('got a notification to change, book id:'+ req.params.id)
+        res.status(200).send("Book was successfully updated")
+    } catch (e) {
+    	console.log(e)
+        res.status(500).send(e)
+    }
+
+})
+module.exports= router
